adc171b5d0afa39038ebddf82a1e3624fe5a4a55
Ari Jort <arijort@gmail.com>
2020-02-26 00:20:58 -0500

    Initial commit for one-away.

---
+++ ctci/one-away.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import unittest
+
+class Solution():
+  def oneaway(self, a, b):
+    """ Given two strings and the set of three rules of string changes, return whether the 2 strings are one edit or zero edits away from each other.
+    Three permissible edits are as follows: single leter inserts, deletions and replacements. """
+    if a == b:
+      return True # handle zero edit check
+    alen, blen = len(a), len(b)
+    if abs(alen - blen) > 1:
+      return False # handle cse where string length difference implies more than 1 edit/delete
+    found, samelength = False, False
+    if alen == blen:
+      samelength = True
+    # assumption: deletion of a longer char from a longer string is the opposite operation to an insert .
+    # Therefor is we categorize strings as longer and shorte we can reduce the number of operations to check down to 2.
+    if alen > blen:
+      shorter, longer = b, a
+    else:
+      shorter, longer = a, b
+    
+    for n, c in enumerate(shorter):
+      if shorter[n] == longer[n]:
+        continue
+      elif samelength:
+        if found:
+          return False
+        else:
+          found = True
+          continue
+      elif shorter[n] == longer[n+1]:
+        if not found:
+          found = True
+      else:
+        return False
+    return True
+
+class Test(unittest.TestCase):
+  def test_oneaway(self):
+    ts = Solution()
+    self.assertTrue(ts.oneaway("pale", "ple"))
+    self.assertTrue(ts.oneaway("pales", "pale"))
+    self.assertTrue(ts.oneaway("pale", "bale"))
+    self.assertFalse(ts.oneaway("pale", "bake"))
+
+if __name__ == '__main__':
+  unittest.main()
