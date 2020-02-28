da33497b16747f35b840d026bd1a36745f76fab3
Ari Jort <arijort@gmail.com>
2020-02-28 16:24:17 -0500

    Added remove_dupes and remove_kth functions.

--- algos/llnodes.py
+++ algos/llnodes.py
@@ -9,9 +9,18 @@ class LinkedListNode():
   def __init__(self, string):
     self.s = string
 
+  def mk_list(self):
+    """ Return a list of values in the linked list from the current node to the end. """
+    l, n = list(), self
+    l.append(self.s)
+    while not n.next == None:
+      l.append(n.next.s)
+      n = n.next
+    return l
+
   def display(self):
     """ Display values in linked-list nodes from this node until the end. Print self then use next to continue recursively. """
-    print(self.s)
+    print(f"node: {self.s}")
     if not self.next == None:
       self.next.display()
 
@@ -26,15 +35,50 @@ class LinkedListNode():
 
   def remove_from_middle(self, s):
     """ Given a string, find the node in the current linked list.  """
-    pass
+    if self.next == None:
+      return False # We are in fact at the end of the list so do nothing.
+    self.s = self.next.s
+    self.next = self.next.next
+    return True
 
-class Solution():
-  def foo(self):
-    pass
+  def remove_dupes(self):
+    """ Given an unsorted singly linked list remove any duplicate nodes. As a secondary: can it be done without any additional data structure. """
+    st = set()
+    n,head = self,self
+    print("before dedupe")
+    head.display()
+    st.add(n.s)
+    while not n.next == None:
+      if n.next.s in st:
+        n.next = n.next.next
+      else:
+        st.add(n.next.s)
+        n = n.next
+    return head
+
+  def remove_kth(self, k):
+    n, head, ct = self, self, 1
+    while not n.next == None:
+      ct += 1
+      n = n.next
+    print(f"found length {ct} will remove node {ct - k}")
+    runner = 0
+    n = head # reinitialize node pointer
+    while runner < ct - k - 1:
+      print(f"runner {runner}")
+      n = n.next
+      runner += 1
+    n.next = n.next.next
+    self.display()
+
+    return head
+
+  def remove_this(self):
+    """ Given pointer to a node in the middle of linked list, remove the current node keeping the rest of the list intact. """
+
 
 class Test(unittest.TestCase):
   def test_llnodes(self):
-    ts = Solution()
     llnode = LinkedListNode("a")
     b = llnode.appendToTail("b")
     a = llnode.appendToTail("a")
@@ -42,9 +86,24 @@ class Test(unittest.TestCase):
     z = llnode.appendToTail("z")
     bar = llnode.appendToTail("bar")
     foo = llnode.appendToTail("foo")
-    llnode.display()
-    zxy.display()
-    self.assertTrue(True)
+
+
+    llnode.remove_dupes()
+    arr = llnode.mk_list()
+    self.assertEqual(arr, ['a', 'b', 'zxy', 'z', 'bar', 'foo']) # test deduping
+    llnode.remove_kth(2)
+    arr = llnode.mk_list()
+    self.assertEqual(arr, ['a', 'b', 'zxy', 'z', 'foo']) # test removeing kth element
+
+    llnode.appendToTail("bar")
+    llnode.appendToTail("b")
+    llnode.appendToTail("c")
+    quz = llnode.appendToTail("quz")
+    llnode.appendToTail("qux")
+    llnode.appendToTail("zoqpp")
+    quz.remove_this()
+    arr = llnode.mk_list()
+    self.assertEqual(arr, ['a', 'b', 'zxy', 'z', 'foo', 'bar', 'b', 'c', 'qux', 'zoqpp']) # test removeing from the middle
 
 if __name__ == '__main__':
   unittest.main()
