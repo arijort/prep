d6698cf95534c710e59d4ef3180465f01277e311
Ari Jort <arijort@gmail.com>
2020-03-04 16:09:15 -0500

    Implemented a triple stack using an array as the basis data structure.

--- ctci/stacks.py
+++ ctci/stacks.py
@@ -2,6 +2,52 @@
 import unittest
 #from pprint import pprint
 
+class ThreeStackArray():
+  """ Implement 3 stacks in a single array. """
+  def __init__(self):
+    """ Use the slots mod 3 to represent the three stacks. I.e. the first stack's nodes will be in array slots 0, 3, 6 etc.
+    The second in 1, 4, 7 etc and the third in 2, 5, 8 etc. """
+    self.array = [None, None, None] # initialization of the three stacks
+    self.head = [-3, -2, -1]
+    self.numstacks = 3
+
+  def pop(self, n):
+    """ Basic interface for the 3 stack includes an integer indicating whether we want to pop from stack 1, 2 or 3. """
+    if not n < self.numstacks and n >= 0:
+      return None # bad n value
+    if self.head[n] == -1:
+      return None # stack is empty
+    node = self.array[self.head[n]]
+    self.head[n] -= self.numstacks
+    return node
+
+  def peek(self, n):
+    return array[head[n]]
+
+  def push(self, n, data):
+    newslot = self.head[n] + self.numstacks
+    self.grow_until(newslot)
+    self.array[newslot] = data
+    self.head[n] = newslot
+
+  def grow_until(self, length):
+    if len(self.array) >= length + 1:
+      return
+    adds = length - len(self.array) + 1
+    for _ in range(adds):
+      self.array.append(None)
+
+  def is_empty(self,n):
+    return self.head[n] < 0
+
+  def mk_list(self, n):
+    """ Return an array containing each item in the stack with the given integer value. """
+    result = []
+    #for i in range(self.head[n], 0, -3):
+    for i in self.array[self.head[n]::-3]:
+      result.append(i)
+    return result
+
 class Stack():
   """ Python implementation of a stack: push onto stack, pop from stack, peek into Stack.
     Depends on private class StackNode. """
@@ -103,6 +149,23 @@ class Test(unittest.TestCase):
     istk.push(-40)
     themin = istk.popmin()
     self.assertEqual(themin.data, -40)
+
+  def test_three_stacks(self):
+    stk3 = ThreeStackArray()
+    stk3.push(0, "a")
+    stk3.push(1, "foob")
+    stk3.push(0, "b")
+    stk3.push(1, "barm")
+    stk3.push(0, "c")
+    stk3.push(1, "quuz")
+    self.assertTrue(stk3.is_empty(2))
+    stk3.push(2, 99)
+    stk3.push(2, 79)
+    stk3.push(2, 44)
+    arr = stk3.mk_list(0)
+    self.assertEqual(arr, ['c', 'b', 'a'])
+    self.assertFalse(stk3.is_empty(0))
+    self.assertFalse(stk3.is_empty(1))
 
 if __name__ == '__main__':
   unittest.main()
