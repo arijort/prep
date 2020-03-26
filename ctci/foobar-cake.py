5a30b5012100f0284df09db1547870092a3198cd
Ari Jort <arijort@gmail.com>
2020-03-25 21:30:59 -0400

    Small enhancement to foobar cake.

--- ctci/foobar-cake.py
+++ ctci/foobar-cake.py
@@ -3,15 +3,14 @@ import unittest
 
 def solution(s):
   l = len(s)
-  max_slices = 1
   for divisor in range(l, 0, -1): 
     if l % divisor != 0: # skip this divisor if we can't make equal sized slices
       continue
     size_slice = int(l / divisor)
     cake_segments = [ s[i:i+size_slice] for i in range(0,l,size_slice) ]
     if len(set(cake_segments)) == 1:
-      max_slices = max(max_slices, divisor)
-  return max_slices
+      return divisor # this is a short-circuit return
+  return divisor # divisor finishes on 1 so we return 1 here
 
 class Test(unittest.TestCase):
   def test_foobar_cake(self):
