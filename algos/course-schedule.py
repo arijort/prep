4f29f78372d19c2bbb15ebc532e247af21492bad
Ari Jort <arijort@gmail.com>
2020-03-29 21:31:45 -0400

    Add 2 implementations of course schedule checking: dfs and topological sort.

--- algos/course-schedule.py
+++ algos/course-schedule.py
@@ -12,12 +12,68 @@ class Solution():
     course_dict = defaultdict(list)
     for course, prereq in courses:
       course_dict[course].append(prereq)
-      #adj_list[course].append(prereq)
-      if not start_course:
-        start_course = course
-    print(f"made course_dict {course_dict}")
+    return True
 
-        
+  def can_finish_dfs(self, n, courses):
+    self.graph = defaultdict(list)
+    self.visiting = set()
+    self.visited = set()
+    def do_dfs(course):
+      if course in self.visited:
+        print(f"    doing course {course} found visited return false")
+        return False
+      if course in self.visiting:
+        print(f"    doing course {course} found cycle")
+        return True # found a cycle
+      self.visiting.add(course)
+      for prereq in self.graph[course]:
+        if do_dfs(prereq):
+          print(f"    doing course {course} returning false")
+          return False
+      self.visiting.remove(course)
+      self.visited.add(course)
+
+    for course, prereq in courses:
+      self.graph[course].append(prereq)
+    print(f"dfs made graph {self.graph}")
+    for course in range(n):
+      if do_dfs(course): # dfs found a cycle so return false
+        print(f"  dfs found cycle on course {course}")
+        return False
+    return True
+
+  def can_finish_topo_sort2(self, n, course_pairs):
+    return len(self.can_finish_topo_sort_find_order(n,course_pairs)) > 0
+
+  def can_finish_topo_sort_find_order(self, n, course_pairs):
+    child =  defaultdict(set)
+    parent = defaultdict(int)
+    for course, prereq in course_pairs:
+      child[prereq].add(course)
+      parent[course] += 1
+
+    q = deque()
+    for i in range(n):
+      if parent[i] == 0:
+        del parent[i]
+        q.append(i)
+    if not q:
+      return []
+    res = []
+    while q:
+      course = q.popleft()
+      res.append(course)
+      for c in child[course]:
+        parent[c] -= 1
+        if parent[c] == 0:
+          del parent[c]
+          q.append(c)
+    print(f" have result {res}")
+    if len(res) == n:
+      return res
+    else:
+      return []
+
 
   def can_finish_topo_sort(self, n, course_pairs):
     """ Use topological sort to derive whether we can complete course work. """
@@ -61,13 +117,26 @@ class Test(unittest.TestCase):
   def test_course_schedule(self):
     ts = Solution()
 
-    trues =  [ (1, [] ), (2, [[1,0]] ), (3, [[1,2],[2,0]])  ]
+    trues =  [ (7, [[1,0], [2,0], [3,1], [4,1], [5,2], [6,2]] ), (1, [] ), (2, [[1,0]] ), (3, [[1,2],[2,0]])  ]
     falses = [ (4, [[2,0],[1,0],[3,1],[3,2],[1,3]] ), (2, [[1,0],[0,1]] ) ]
 
     #[ self.assertTrue(  ts.canFinish(n,courses) ) for n, courses in trues ]
-    [ self.assertTrue(  ts.can_finish_topo_sort(n,courses) ) for n, courses in trues ]
+    #[ self.assertTrue(  ts.can_finish_topo_sort(n,courses) ) for n, courses in trues ]
+    [ self.assertTrue(  ts.can_finish_topo_sort2(n,courses) ) for n, courses in trues ]
+    [ self.assertTrue(  ts.can_finish_dfs(n,courses) ) for n, courses in trues ]
     #[ self.assertFalse( ts.canFinish(n,courses) ) for n, courses in falses ]
-    [ self.assertFalse( ts.can_finish_topo_sort(n,courses) ) for n, courses in falses ]
+    #[ self.assertFalse( ts.can_finish_topo_sort(n,courses) ) for n, courses in falses ]
+    [ self.assertFalse( ts.can_finish_topo_sort2(n,courses) ) for n, courses in falses ]
+    [ self.assertFalse( ts.can_finish_dfs(n,courses) ) for n, courses in falses ]
+
+  def test_find_course_order(self):
+    course_list = [[1,0]]
+    course_list4 = [[1,0],[2,0],[3,1],[3,2]]
+    course_list7 = [[1,0], [2,0], [3,1], [4,1], [5,2], [6,2] ]
+    ts = Solution()
+    self.assertEqual(ts.can_finish_topo_sort_find_order(2, course_list), [0,1] )
+    self.assertEqual(ts.can_finish_topo_sort_find_order(4, course_list4), [0,1,2,3] )
+    self.assertEqual(ts.can_finish_topo_sort_find_order(7, course_list7), [0,1,2,3,4,5,6] )
 
 if __name__ == '__main__':
   unittest.main()
